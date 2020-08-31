f6916b06362279133296d1c08140fc74d48eb1b2
Kodimpi Joonas <joonaskilpela@gmail.com>
2020-08-31 14:53:31 +0300

    Player moves and rotates.
    Attempting to make the camera follow the player.

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    Transform player;
+    public float speed = 200;
+
+    private void Start()
+    {
+        player = Transform.FindObjectOfType<PlayerMovement>().transform;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 velocity = Vector3.zero;
+        Vector3 forward = player.transform.forward * speed;
+        Vector3 needPos = player.transform.position - forward;
+
+        transform.position = Vector3.SmoothDamp(transform.position, needPos, ref velocity, 0.5f);
+        //transform.rotation = player.rotation;
+        transform.LookAt(player);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 10;
+    public float rotSpeed = 5;
+    Transform cam;
+
+    private void Start()
+    {
+        cam = Transform.FindObjectOfType<CameraFollow>().transform;
+    }
+    private void FixedUpdate()
+    {
+        float forward = Input.GetAxis("Vertical");
+        float side = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * forward * speed * Time.fixedDeltaTime);
+        transform.Rotate(transform.rotation.x, (side * rotSpeed), transform.rotation.z);
+    }
+}
