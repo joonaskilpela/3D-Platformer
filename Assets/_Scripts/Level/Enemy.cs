94b29187ffcb0a7ef997c747d9a590cb08f21c1e
joonaskilpela <joonaskilpela@gmail.com>
2020-09-05 20:27:59 +0300

    Added enemies and enemy behaviour
    Enemies push the Player back when touched
    Enemies keep their eye on Player when he's close
    Blob enemy dies when jumped on

---
+++ Assets/_Scripts/Level/Enemy.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public enum EnemyType { blob, spiky };
+    public EnemyType myType;
+    Animator anim;
+    float knockBack = 1f;
+    PlayerMovement player;
+    Transform plyTransform;
+    float distance;
+    Animation ama;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        player = FindObjectOfType<PlayerMovement>();
+        plyTransform = player.transform;
+        ama = GetComponent<Animation>();
+    }
+
+    private void Update()
+    {
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Die"))
+        {
+            distance = Vector3.Distance(transform.position, plyTransform.position);
+            if (distance < 10)
+            {
+                anim.SetBool("SeeingSomething", true);
+                transform.LookAt(plyTransform.position);
+            }
+            else
+            {
+                anim.SetBool("SeeingSomething", false);
+            }
+        }
+        else
+            transform.rotation = Quaternion.identity;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Die"))
+        {
+            if (other.GetComponent<PlayerMovement>())
+            {
+                if (myType == EnemyType.blob)
+                {
+                    if (other.transform.position.y > transform.position.y)
+                    {
+                        anim.SetBool("Dying", true);
+                        GetComponent<BoxCollider>().enabled = false;
+                        GetComponent<SphereCollider>().enabled = true;
+                        other.GetComponent<PlayerMovement>().yVelocity = other.GetComponent<PlayerMovement>().jumpForce;
+                    }
+                    else
+                    {
+                        //Vector3 dir = other.transform.position - transform.position;
+                        //other.GetComponent<CharacterController>().Move(dir * knockBack);
+                        other.GetComponent<PlayerMovement>().knockBackForce = knockBack;
+                        other.GetComponent<PlayerMovement>().pushingBack = true;
+                    }
+                }
+                else if (myType == EnemyType.spiky)
+                {
+                    other.GetComponent<PlayerMovement>().knockBackForce = knockBack;
+                    other.GetComponent<PlayerMovement>().pushingBack = true;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 10;
+    public float rotSpeed = 5;
+    [HideInInspector]
+    public bool moving;
+    [HideInInspector]
+    public bool rotating;
+    Transform cam;
+    BoxCollider boxCollider;
+    Rigidbody rb;
+    bool grounded;
+    bool jumping = false;
+    public float gravity = 2;
+    Vector3 velocity;
+    Vector3 eulerAngleVelocity;
+    CharacterController cc;
+    [HideInInspector]
+    public float yVelocity = 0;
+    public float jumpForce = 2;
+    float multiplier = 0;
+    public float accelerationSpeed = 2;
+    Animator anim;
+    [HideInInspector]
+    public bool pushingBack = false;
+    [HideInInspector]
+    public float knockBackForce = 1;
+
+    private void Start()
+    {
+        cam = Transform.FindObjectOfType<CameraFollow>().transform;
+        boxCollider = GetComponent<BoxCollider>();
+        rb = GetComponent<Rigidbody>();
+        cc = GetComponent<CharacterController>();
+        velocity = Vector3.zero;
+        anim = GetComponentInChildren<Animator>();
+    }    
+
+    private void Update()
+    {
+        //movement.x = 0;
+        //movement.z = 0;
+        if(Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Vertical") > 0)
+        {
+            if (multiplier < 1)
+                multiplier += Time.smoothDeltaTime;
+            if (multiplier >= 1)
+                multiplier = 1;
+        }
+        else
+        {
+            if (multiplier > 0)
+                multiplier -= Time.smoothDeltaTime;
+            if (multiplier <= 0)
+                multiplier = 0;
+        }
+        Vector3 targetVelocity = Vector3.zero;
+        targetVelocity += cam.forward * Input.GetAxis("Vertical");
+        targetVelocity += cam.right * Input.GetAxis("Horizontal");
+        //movement.Normalize();
+        targetVelocity *= speed * Time.smoothDeltaTime;
+        velocity = Vector3.Lerp(velocity, targetVelocity, accelerationSpeed * Time.smoothDeltaTime);
+        Vector3 aVel = velocity;
+        aVel.y = 0;
+        anim.SetFloat("MoveSpeed", aVel.magnitude * 5);
+        anim.SetBool("Grounded", cc.isGrounded);
+        if (!cc.isGrounded)
+        {
+            yVelocity -= gravity * Time.smoothDeltaTime;
+        }
+        else
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                yVelocity = jumpForce;
+            }
+        }
+        velocity.y = yVelocity;
+    }
+
+    void MoveCharacter(Vector3 direction)
+    {
+        cc.Move(direction);
+        Vector3 vel = cc.velocity;
+        vel.y = 0;
+        if (vel.magnitude > 0)
+        {
+            transform.rotation = Quaternion.LookRotation(vel);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MoveCharacter(velocity);
+        if (pushingBack)
+        {
+            PushBack(knockBackForce);
+        }
+    }
+
+    public void PushBack(float force)
+    {
+        Vector3 dir = transform.forward * -force * Time.smoothDeltaTime;
+        dir.z = -1;
+        dir.y += 0.1f;
+        //dir.Normalize();
+        cc.Move(dir);
+        //transform.Translate(dir);
+        if(force > 0.5f)
+        {
+            knockBackForce *= 0.8f;
+        }
+        else
+        {
+            pushingBack = false;
+        }
+    }
+}
